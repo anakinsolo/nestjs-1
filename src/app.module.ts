e599bda22c0e01776b8fdd4d621b7186ccdfc889
Tuan Dinh <darthvader0510@gmail.com>
2022-02-21 03:07:32 +0700

    Added mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { CatsController } from './cats.controller';
 import { AppService } from './app.service';
 import { FoodModule } from './foods/foods.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [FoodModule],
+  imports: [FoodModule, MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController, CatsController],
   providers: [AppService],
 })
